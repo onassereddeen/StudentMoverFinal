f2811cdf941bb89e36f2b9eaa8d4486bf1c16a35
CCASSELL20 <christiancassell@icloud.com>
2023-05-05 23:02:32 -0400

    password doc

--- app/src/main/java/com/zybooks/trips/MainActivity.java
+++ app/src/main/java/com/zybooks/trips/MainActivity.java
@@ -6,13 +6,27 @@ package com.zybooks.trips;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
 
     // Declare Variables
     Button signIn;
+    boolean validLog = false;
 
 
 
@@ -26,17 +40,50 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the button reference
         signIn = findViewById(R.id.signInButton);
+        EditText passText = findViewById(R.id.passwordEditText);
+        EditText userText = findViewById(R.id.emailEditText);
 
-        // Perform setOnClickListener event on sign in button
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Launch the Dashboard Activity
-                Intent intent = new Intent(MainActivity.this, Dashboard.class);
-                startActivity(intent);
+                // get text in the edit text inputs
+                String text1 = userText.getText().toString();
+                String text2 = passText.getText().toString();
+                // Run search file methods and make sure the credentials are correct
+                // If the login is incorrect print a message prompting the user to enter valid input
+                if (searchFile("credentials.txt", text1) && !text1.equals("")) {
+                    if (searchFile("passwords.txt", text2) && !text2.equals("")) {
+                        Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Enter Valid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Enter Valid Credentials", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
+        }
+    private boolean searchFile(String fileName, String searchQuery) {
+        // Use try and catch blocks when opening files
+        try {
+            // Accesses asset manager files and creates a reader.
+            AssetManager assetManager = getAssets();
+            InputStream inputStream = assetManager.open(fileName);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            // Reads all lines in the text file until the credentials are found.
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(searchQuery)) {
+                    return true;
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // If the correct user input is not found return false.
+        return false;
     }
 
 }
