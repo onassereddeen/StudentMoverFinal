30e3e9b968b2830d28aa91d9bd00752c6bd67f0b
Omar <onassereddeen@rollins.edu>
2023-05-05 21:37:55 -0400

    updated faux data

--- app/src/main/java/com/zybooks/trips/TripHolder.java
+++ app/src/main/java/com/zybooks/trips/TripHolder.java
@@ -4,11 +4,11 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,15 +32,46 @@ public class TripHolder extends RecyclerView.ViewHolder {
         seatsFilled = itemView.findViewById(R.id.seat);
         request = itemView.findViewById(R.id.requestTrip);
 
+        // Add a tag to the request button to associate it with this item's position in the RecyclerView
+        request.setTag(getAdapterPosition());
+
         request.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(rootView.getContext(), "Trip Requested", Toast.LENGTH_LONG).show();
+                // Retrieve the position of this item in the RecyclerView
+                int position = (int) v.getTag();
+
+                // Create and show the dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getRootView().getContext());
+                builder.setTitle("Request Confirmation");
+                builder.setMessage("Would you like to request this ride?");
+
+                // Add positive button
+                builder.setPositiveButton("Request", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Positive button clicked
+                    }
+                });
+
+                // Add negative button
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Negative button clicked
+                    }
+                });
+
+                // Create and show the dialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+                // Center the dialog on the screen
+                Window window = dialog.getWindow();
+                if (window != null) {
+                    window.setGravity(Gravity.CENTER);
+                }
             }
         });
-
-
-
     }
-
 }
